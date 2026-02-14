4a9cddb69306b10248efc9d93b291d93335f150a
= <=>
2026-02-14 19:41:30 +0000

    Enhance email templates and configuration for Rooted Faith Church. Added INSTAGRAM_URL to docker-compose.yml and updated entrypoint and render-emails scripts to include it in email rendering. Updated email HTML files to use Anek Gujarati font family and adjusted background colors for consistency.

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,7 @@ RENDER_ROOT="/tmp/html"
 # Escape characters that are special in sed replacement.
 PUBLIC_URL_ESCAPED=$(printf '%s' "${PUBLIC_URL}" | sed -e 's/[\/&|]/\\&/g')
 FACEBOOK_URL_ESCAPED=$(printf '%s' "${FACEBOOK_URL:-#}" | sed -e 's/[\/&|]/\\&/g')
+INSTAGRAM_URL_ESCAPED=$(printf '%s' "${INSTAGRAM_URL:-#}" | sed -e 's/[\/&|]/\\&/g')
 
 # Prepare a writable copy of the site content.
 rm -rf "${RENDER_ROOT}"
@@ -18,7 +19,7 @@ cp -R "${SRC_ROOT}" "${RENDER_ROOT}"
 # Render email templates with the injected PUBLIC_URL placeholder.
 if [ -d "${RENDER_ROOT}/emails" ]; then
   find "${RENDER_ROOT}/emails" -name '*.html' -print0 | while IFS= read -r -d '' file; do
-    sed -e "s|__PUBLIC_URL__|${PUBLIC_URL_ESCAPED}|g" -e "s|__FACEBOOK_URL__|${FACEBOOK_URL_ESCAPED}|g" "${file}" > "${file}.tmp"
+    sed -e "s|__PUBLIC_URL__|${PUBLIC_URL_ESCAPED}|g" -e "s|__FACEBOOK_URL__|${FACEBOOK_URL_ESCAPED}|g" -e "s|__INSTAGRAM_URL__|${INSTAGRAM_URL_ESCAPED}|g" "${file}" > "${file}.tmp"
     mv "${file}.tmp" "${file}"
   done
 fi
